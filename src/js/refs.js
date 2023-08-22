5b0afa838a428555d6b9e815dc919cb31920d60a
KaterynaChaplynska <101507992+KaterynaChaplynska@users.noreply.github.com>
2023-08-23 01:33:12 +0300

    delete the repository

--- src/js/refs.js
+++
@@ -1,6 +0,0 @@
-const refs = {
-  form: document.querySelector('#search-form'),
-  galleryRef: document.querySelector('.gallery'),
-  guard: document.querySelector('.js-guard'),
-};
-export default refs;
--- src/js/search.js
+++
@@ -1,5 +0,0 @@
-const searchIcon = document.querySelector('.search-icon__wrapper');
-
-searchIcon.addEventListener('click', e =>
-  searchIcon.parentElement.classList.toggle('open')
-);
